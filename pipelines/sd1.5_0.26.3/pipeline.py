0156cab3a68243dc3d7a17d6fc9bd8430854e8b4
BlueCP <47088138+BlueCP@users.noreply.github.com>
2024-06-06 20:46:57 +0100

    Fix

--- pipelines/sd1.5_0.26.3/pipeline.py
+++ pipelines/sd1.5_0.26.3/pipeline.py
@@ -25,7 +25,7 @@ from diffusers.configuration_utils import FrozenDict
 from diffusers.image_processor import PipelineImageInput, VaeImageProcessor
 from diffusers.loaders import FromSingleFileMixin, IPAdapterMixin, LoraLoaderMixin, TextualInversionLoaderMixin
 from diffusers.models import AutoencoderKL, ImageProjection
-from ..unet_2d_condition import UNet2DConditionModel
+from .unet_2d_condition import UNet2DConditionModel
 from diffusers.models.attention_processor import FusedAttnProcessor2_0
 from diffusers.models.lora import adjust_lora_scale_text_encoder
 from diffusers.schedulers import KarrasDiffusionSchedulers
